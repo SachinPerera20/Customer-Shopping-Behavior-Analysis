5c59ea9c388dafe5e139068211c1a950d4a158ae
SachinPerera20 <sachinnipun20@gmail.com>
2026-04-19 21:06:53 +0530

    Data base Query added

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -4,3 +4,4 @@ SELECT TOP 100 * from customer
 
 SELECT * from customer where item_purchased = 'Shirt' and size = 'L' ;
 
+SELECT top 50 * from customer where gender = 'Female' and age_group = 'Middle Aged' ;
